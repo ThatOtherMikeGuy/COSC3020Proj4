3dc81dc5eb530c642884e00f0186693be36474f0
zephconnell <zephconnell@gmail.com>
2017-04-29 11:55:12 -0600

    Structure
    just files

---
+++ SkipList.h
@@ -0,0 +1,20 @@
+// Zephaniah Connell, Michael Schwindt, and Marcus Rieker
+// COSC 3020 Project 4
+// Skip List: Search, Insert, Delete
+// SkipList.h
+// 04-29-17
+
+using namespace std;
+
+#include <vector>;
+#include <list>;
+
+class SkipList
+{
+	private vector<list> structure;
+	
+	public SkipList()
+	{
+
+	}
+};
---
+++ SkipListDriver.cpp
@@ -0,0 +1,10 @@
+// Zephaniah Connell, Michael Schwindt, and Marcus Rieker
+// COSC 3020 Project 4
+// Skip List: Search, Insert, Delete
+// SkipListDriver.cpp
+// 04-29-17
+
+main()
+{
+	return 0;
+}
